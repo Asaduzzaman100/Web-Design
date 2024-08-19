a93a279d9b0feb73ef68d76bf5de3cb2f2371c34
MD. ASADUZZAMAN <asaduzzaman.swe@gmail.com>
2024-08-19 10:41:47 +0600

    Add files via upload

---
+++ Web Design Beginner/Part_88/js/app.js
@@ -0,0 +1,10 @@
+// const url = 'https://jsonplaceholder.typicode.com/users'
+
+// fetch('https://jsonplaceholder.typicode.com/users/1')
+// .then(res => console.log(res))
+
+// fetch('https://jsonplaceholder.typicode.com/users/1')
+// .then(res => res.json())
+// .then(data => console.log(data))
+
+
