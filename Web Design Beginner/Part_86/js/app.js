0adfe6e3740aa8222294ce389782253b1c2b1b9d
MD. ASADUZZAMAN <asaduzzaman.swe@gmail.com>
2024-08-19 10:39:28 +0600

    Add files via upload

---
+++ Web Design Beginner/Part_86/js/app.js
@@ -0,0 +1,14 @@
+for(i=1; i<=50; i++){
+      if(i%3==0 && i%5==0){
+            console.log("rony & rana");   
+      }
+      else if(i%3==0){
+            console.log("rony");
+      }
+      else if(i%5==0){
+            console.log("rana");
+      }
+      else{
+            console.log(i);
+      }
+}
